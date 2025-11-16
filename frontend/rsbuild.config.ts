f8393deafbaf0e597dafb13e8db92bea8c32aca9
iimuz <iz.primary.story+github@gmail.com>
2025-11-16 14:56:47 +0900

    build: :building_construction: frontend の package, tsconfig, rsbuild を追加

---
+++ frontend/rsbuild.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from '@rsbuild/core';
+import { pluginReact } from '@rsbuild/plugin-react';
+
+export default defineConfig({
+  plugins: [pluginReact()],
+  html: {
+    template: './index.html',
+  },
+  source: {
+    entry: {
+      index: './src/main.tsx',
+    },
+  },
+  server: {
+    port: 3000,
+    open: true,
+  },
+});
